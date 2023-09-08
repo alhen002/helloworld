a212928ef23043965135af707a5284fe9eceb1f6
Alexander Henting <a.henting@posteo.de>
2023-09-08 11:27:56 +0200

    Hello World initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+// components
+import Heading from "./components/Heading";
+
+export default function App() {
+  return (
+    <main>
+      <Heading>Hello World</Heading>
+    </main>
+  );
+}
